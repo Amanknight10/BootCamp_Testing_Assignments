515a2d67fcea453ab2f66f8df4968a4aeb3445d7
Amanknight10 <amanknight10@gmail.com>
2026-07-29 01:08:04 +0530

    Add files via upload

---
+++ Automation_Testing/Scanner Class/ScannerClass/src/ScannerDemo/ScannerClass.java
@@ -0,0 +1,9 @@
+package ScannerDemo;
+import java.util.Scanner;
+public class ScannerClass {
+   public static void main(string[] args) {
+	   Scanner keyboard=new Scanner(System.in);
+	   System.out.println("Enter your first name initial");
+	   String FirstIntial
+   }
+}
