3f0c3083d334bf279a1f10799c0d5eb9686cb776
Amanknight10 <amanknight10@gmail.com>
2026-07-29 00:19:05 +0530

    Add files via upload

---
+++ Automation_Testing/Java Access Modifiers/Java_Access_Modifier/src/com/AccessModifiers/Examples/C_03_Protected.java
@@ -0,0 +1,12 @@
+package com.AccessModifiers.Examples;
+
+public class C_03_Protected extends B {
+    C
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int output=obj.z;
+        System.out.println("value of z is:"+output);
+        obj.ProtectedMethod();
+        }
+
+}
---
+++ Automation_Testing/Java Access Modifiers/Java_Access_Modifier/src/com/AccessModifiers/Examples/C_04_public.java
@@ -0,0 +1,10 @@
+package com.AccessModifiers.Examples;
+
+public class C_04_public extends B {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        
+	}
+
+}
